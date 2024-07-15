f6c8b63c0bb8104fd87cf2921d32161d12cea6a3
NestyTampubolon <76415637+NestyTampubolon@users.noreply.github.com>
2024-07-15 21:59:41 +0700

    Update AuthenticationController.java

--- src/main/java/com/pilotlog/pilottrainingmanagement/controller/AuthenticationController.java
+++ src/main/java/com/pilotlog/pilottrainingmanagement/controller/AuthenticationController.java
@@ -11,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.beans.factory.annotation.Autowired;
+
+@CrossOrigin(origins="http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
